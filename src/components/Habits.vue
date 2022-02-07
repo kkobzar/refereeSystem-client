f09dab9463b4301cf4906af8c70e8d537f10f4ce
Kiril Kobzar <kirilkobzar@gmail.com>
2022-02-07 13:22:24 +0100

    HabitItem component

--- src/components/Habits.vue
+++ src/components/Habits.vue
@@ -4,19 +4,25 @@
       <h2>No habits yet!</h2>
     </div>
     <ul v-else>
-      <li v-for="habit in userHabits" :key="habit.id">
+      <HabitItem v-for="habit in userHabits" :key="habit.id"
+                 :title="habit.title"
+                 :question="habit.question"
+      />
+<!--      <li v-for="habit in userHabits" :key="habit.id">
         <span>{{habit.title}}</span>
         <p v-if="habit.question">{{habit.question}}</p>
-      </li>
+      </li>-->
     </ul>
   </div>
 </template>
 
 <script>
 import {mapActions, mapGetters} from "vuex";
+import HabitItem from "@/components/Habits/HabitItem";
 
 export default {
   name: "Habits",
+  components: { HabitItem },
   computed: {
     ...mapGetters(["userHabits"]),
   },
---
+++ src/components/Habits/HabitItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <li class="habit-item">
+    <div v-if="!edited" class="habit-item__info">
+      <p>{{ title }}</p>
+      <p v-if="question">{{ question }}</p>
+      <button @click.prevent="edited = true">Edit</button>
+    </div>
+    <div v-if="edited" class="habit-item__edit">
+      <input type="text" v-model="title" />
+      <input v-if="question" type="text" v-model="question" />
+      <button>Save</button>
+    </div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "HabitItem",
+  props: {
+    title: String,
+    question: String,
+  },
+  data() {
+    return {
+      edited: false,
+    };
+  },
+};
+</script>
+
+<style scoped></style>
