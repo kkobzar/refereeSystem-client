cfef070b12e0439d954ff6b92b3e07d03bd87e23
Kiril Kobzar <kirilkobzar@gmail.com>
2022-02-16 20:13:42 +0100

    selected date custom styles

--- src/components/Habits/HabitInfo.vue
+++ src/components/Habits/HabitInfo.vue
@@ -2,12 +2,13 @@
   <section id="habit-info">
     <a @click="$router.go(-1)">Back</a>
     <v-date-picker
-      color="purple"
       @input="setHabitCheck"
       mode="date"
+      :fillMode="false"
       v-model="checkedDate"
       v-if="loaded"
       :attributes="habitsChecks"
+      :select-attribute="selectAttr"
     />
   </section>
 </template>
@@ -26,26 +27,48 @@ export default {
       loaded: false,
       error: false,
       checkedDate: null,
+      selectAttr: {
+        highlight: {
+          fillMode: "none",
+          color: "purple",
+        },
+        contentStyle: {
+          color: "grey",
+        },
+      },
     };
   },
   async created() {
     let hChecks = await habitService.getHabitChecks(this.$props.id);
     if (!hChecks) this.error = "Can't load data for this habit";
-    this.habitsChecks = [
-      {
-        highlight: {
-          color: "purple",
-          fillMode: "outline",
-        },
-        dates: hChecks,
+    this.habitsChecks.push({
+      highlight: {
+        color: "purple",
+        fillMode: "outline",
       },
-    ];
+      dates: hChecks,
+    });
     this.loaded = true;
   },
   methods: {
     async setHabitCheck() {
-      console.log(this.checkedDate.getTime());
-      if (!await habitService.checkHabit(this.$props.id,this.checkedDate.getTime())){
+      if (
+        await habitService.checkHabit(
+          this.$props.id,
+          this.checkedDate.getTime()
+        )
+      ) {
+        let hChecks = await habitService.getHabitChecks(this.$props.id);
+        this.habitsChecks = [
+          {
+            highlight: {
+              color: "purple",
+              fillMode: "outline",
+            },
+            dates: hChecks,
+          },
+        ];
+      } else {
         this.error = "Can't check habit";
       }
     },
